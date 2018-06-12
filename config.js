85247b0256e2ae1d502aee2895b2708773da4df8
quan <quan_xiaoxiao@qq.com>
2018-06-12 10:29:20 +0800

    xxx

--- config.js
+++ config.js
@@ -1,211 +1,226 @@
-const Handlebars = require('handlebars');
-const shelljs = require('shelljs');
 const fs = require('fs');
 const qs = require('querystring');
-const _ = require('lodash');
 const path = require('path');
+const Handlebars = require('handlebars');
+const shelljs = require('shelljs');
+const _ = require('lodash');
 
 module.exports = {
-  components: () => ({
-    from: path.resolve(__dirname, 'storybook/src/components'),
-    include: [
-      /^DatePicker/,
-      /^Layout/,
-      /^Slider/,
-    ],
-    to: 'src/components',
-    after: (type) => {
-      if (type === 'create') {
-        shelljs.exec('npm install moment');
-      }
-    },
-  }),
-  configs: (ctx) => {
+  'component/:name': ({ params, query }, cb) => {
+    const exclude = [];
+    let to = `src/components/${params.name}`;
+    if (query.name) {
+      exclude.push(/index\.js$/);
+    }
+    if (query.scene) {
+      to = `src/scenes/${query.scene}/components/${params.name}`;
+    }
+    cb({
+      from: path.resolve(__dirname, 'components'),
+      handlePathName: name => name.replace(/__name__(?=\.)/, query.name || params.name),
+      exclude,
+      handleContent: content => Handlebars.compile(content)({
+        name: query.name || params.name,
+      }),
+      to,
+    });
+  },
+  components: (ctx, cb) => {
+    cb({
+      from: path.resolve(__dirname, 'storybook/src/components'),
+      include: [
+        /^DatePicker/,
+        /^Layout/,
+        /^Slider/,
+      ],
+      to: 'src/components',
+      complete: (type) => {
+        if (type === 'create') {
+          shelljs.exec('npm install moment');
+        }
+      },
+    });
+  },
+  configs: ({ query }, cb) => {
     const include = [
       /\.gitignore$/,
       /\.editorconfig$/,
     ];
-    if (ctx.query.type === 'react') {
+    if (query.type === 'react') {
       include.push(/\.react_eslintrc$/);
-      include.push(/package\.json$/);
       include.push(/norice\.config\.js$/);
       include.push(/postcss\.config\.js/);
       include.push(/\.babelrc$/);
     } else {
       include.push(/\.eslintrc$/);
     }
-    return {
+    cb({
       from: path.resolve(__dirname, 'config'),
       handlePathName: a => a.replace(/react_/, ''),
-      handleContent: content => Handlebars.compile(content)(ctx.query),
+      handleContent: content => Handlebars.compile(content)(query),
       include,
       to: '',
-    };
+    });
   },
-  webpack: () => ({
-    from: path.resolve(__dirname, 'webpack'),
-    to: 'webpack',
-  }),
-
-  'react/view/:name': ctx => ({
-    from: path.resolve(__dirname, 'react-view', ctx.params.name === 'View' ? 'View' : 'Page'),
-    exclude: [/\.swp$/],
-    handleContent: content => Handlebars.compile(content)({ name: ctx.params.name }),
-    to: `src/scenes/${ctx.params.name}`,
-  }),
-  'react/:name': ctx => ({
-    from: path.resolve(__dirname, 'react'),
-    exclude: ctx.query.type === 'view' ? [/containers/] : [],
-    handleContent: content => Handlebars.compile(content)({
-      view: ctx.query.type === 'view',
-      name: ctx.params.name,
-      ...ctx.query,
-    }),
-    to: `${ctx.params.name}/src`,
-    after: () => {
-      process.chdir(ctx.params.name);
-      shelljs.exec(`templ create "configs?type=react&name=${ctx.params.name}"`);
-      shelljs.exec('templ create webpack');
-      shelljs.exec('templ create components');
-      if (ctx.query.type === 'view') {
-        shelljs.exec('templ create "react/view/View"');
-      }
-      const dependencies = [
-        'classnames',
-        'lodash',
-        'normalize.css',
-        'prop-types',
-        'react',
-        'react-dom',
-        'react-redux',
-        'redux',
-        'redux-promise',
-        'redux-thunk',
-      ];
-      const devDependencies = [
-        'babel-core',
-        'babel-loader',
-        'babel-preset-env',
-        'babel-preset-react',
-        'babel-preset-stage-0',
-        'css-loader',
-        'extract-text-webpack-plugin@next',
-        'html-webpack-plugin',
-        'postcss-color-function',
-        'postcss-css-variables',
-        'postcss-loader',
-        'postcss-nested',
-        'redux-logger',
-        'style-loader',
-        'url-loader',
-        'react-hot-loader',
-        'webpack',
-        'webpack-cli',
-        'webpack-hot-middleware',
-        'webpack-merge',
-        'babel-plugin-transform-runtime',
-      ];
-      if (ctx.query.type === 'view') {
-        dependencies.push('react-router-dom');
-        dependencies.push('react-router-redux@next');
-        dependencies.push('history');
-        dependencies.push('qs');
-      }
-      shelljs.exec(`npm install ${dependencies.join(' ')}`);
-      shelljs.exec(`npm install --save-dev ${devDependencies.join(' ')}`);
-    },
-  }),
-  'storybook/:name': ctx => ({
-    from: path.resolve(__dirname, 'storybook'),
-    exclude: [/node_modules/, /\.swp$/, /package-lock\.json/],
-    to: `${ctx.params.name}`,
-  }),
-  'component/:name': (ctx) => {
-    const exclude = [];
-    let to = `src/components/${ctx.params.name}`;
-    if (ctx.query.name) {
-      exclude.push(/index\.js$/);
-    }
-    if (ctx.query.scene) {
-      to = `src/scenes/${ctx.query.scene}/components/${ctx.params.name}`;
-    }
-    return {
-      from: path.resolve(__dirname, 'components'),
-      exclude,
-      handlePathName: name => name.replace(/__name__(?=\.)/, ctx.query.name || ctx.params.name),
+  webpack: (ctx, cb) => {
+    cb({
+      from: path.resolve(__dirname, 'webpack'),
+      to: 'webpack',
+    });
+  },
+  'package/react': ({ query }, cb) => {
+    cb({
+      from: path.resolve(__dirname, 'config/package.json'),
+      handleContent: content => Handlebars.compile(content)({ name: query.name }),
+      to: '',
+    });
+  },
+  'react/view/:name': ({ params }, cb) => {
+    cb({
+      from: path.resolve(__dirname, 'react-view', params.name === 'View' ? 'View' : 'Page'),
+      exclude: [/\.swp$/],
+      handleContent: content => Handlebars.compile(content)({ name: params.name }),
+      to: `src/scenes/${params.name}`,
+    });
+  },
+  'react/:name': ({ query, params }, cb) => {
+    cb({
+      from: path.resolve(__dirname, 'react'),
+      exclude: query.type === 'view' ? [/containers/] : [],
       handleContent: content => Handlebars.compile(content)({
-        name: ctx.query.name || ctx.params.name,
+        view: query.type === 'view',
+        name: params.name,
+        ...query,
       }),
-      to,
-    };
-  },
-  'container/:name': (ctx) => {
-    let to = `src/containers/${ctx.params.name}`;
-    if (ctx.query.scene) {
-      to = `src/scenes/${ctx.query.scene}/containers/${ctx.params.name}`;
-    }
-    return {
-      from: path.resolve(__dirname, 'containers'),
-      handlePathName: name => name.replace(/__name__(?=\.)/, ctx.query.name || ctx.params.name),
-      handleContent: content => Handlebars.compile(content)({ name: ctx.params.name }),
-      to,
-    };
-  },
-  'container/component/:name': (ctx) => {
-    let to = `src/containers/${ctx.params.name}`;
-    if (ctx.query.scene) {
-      to = `src/scenes/${ctx.query.scene}/containers/${ctx.params.name}`;
-    }
-    return {
-      from: path.resolve(__dirname, 'containers'),
-      handlePathName: name => name.replace(/__name__(?=\.)/, ctx.query.name || ctx.params.name),
-      handleContent: content => Handlebars.compile(content)({ name: ctx.params.name }),
-      to,
-      after: () => {
-        shelljs.exec(`templ create component/${ctx.params.name}?${qs.stringify(ctx.query)}`);
+      to: `${params.name}/src`,
+      complete: () => {
+        process.chdir(params.name);
+        shelljs.exec(`tpl create package/react?name=${params.name}`);
+        shelljs.exec('tpl create configs?type=react');
+        shelljs.exec('tpl create webpack');
+        shelljs.exec('tpl create components');
+        if (query.type === 'view') {
+          shelljs.exec('tpl create react/view/View');
+        }
+        const dependencies = [
+          'classnames',
+          'lodash',
+          'normalize.css',
+          'prop-types',
+          'react',
+          'react-dom',
+          'react-redux',
+          'redux',
+          'redux-promise',
+          'redux-thunk',
+        ];
+        const devDependencies = [
+          'babel-core',
+          'babel-loader',
+          'babel-preset-env',
+          'babel-preset-react',
+          'babel-preset-stage-0',
+          'css-loader',
+          'extract-text-webpack-plugin@next',
+          'html-webpack-plugin',
+          'postcss-color-function',
+          'postcss-css-variables',
+          'postcss-loader',
+          'postcss-nested',
+          'redux-logger',
+          'style-loader',
+          'url-loader',
+          'react-hot-loader',
+          'webpack',
+          'webpack-cli',
+          'webpack-hot-middleware',
+          'webpack-merge',
+          'babel-plugin-transform-runtime',
+        ];
+        if (query.type === 'view') {
+          dependencies.push('react-router-dom');
+          dependencies.push('react-router-redux@next');
+          dependencies.push('history');
+          dependencies.push('qs');
+        }
+        shelljs.exec(`npm install ${dependencies.join(' ')}`);
+        shelljs.exec(`npm install --save-dev ${devDependencies.join(' ')}`);
       },
-    };
+    });
   },
-  'reducer/:name': ctx => ({
-    from: path.resolve(__dirname, 'reducer/reducer.js'),
-    handlePathName: name => (ctx.query.scene ? name : `${ctx.params.name}.js`),
-    to: ctx.query.scene ?
-      `src/scenes/${ctx.query.scene}/data/${ctx.params.name}` :
-      'src/reducers',
-    after: () => {
-      const query = {
-        list: ctx.query.scene ?
-          fs.readdirSync(`src/scenes/${ctx.query.scene}/data`) :
-          fs.readdirSync('src/reducers')
-            .filter(a => a !== 'index.js' && !/\.swp$/.test(a))
-            .map(a => path.basename(a, '.js')),
-        scene: ctx.query.scene,
-      };
-      shelljs.exec(`templ update 'rootReducer?${qs.stringify(query)}'`);
-    },
-  }),
-  rootReducer: ctx => ({
-    from: path.resolve(__dirname, 'reducer/rootReducer.js'),
-    handlePathName: () => (ctx.query.scene ? 'reducer.js' : 'index.js'),
-    handleContent: content => Handlebars.compile(content)({
-      scene: ctx.query.scene,
-      list: (Array.isArray(ctx.query.list) ? // eslint-disable-line
-        ctx.query.list : _.isEmpty(ctx.query.list) ? [] : [ctx.query.list]).map(a => ({
-        reducer: a,
-      })),
-    }),
-    to: ctx.query.scene ? `src/scenes/${ctx.query.scene}` : 'src/reducers',
-  }),
-  'action/:name': ctx => ({
-    from: path.resolve(__dirname, 'reducer/actions.js'),
-    handleContent: content => Handlebars.compile(content)({
-      name: ctx.params.name,
-      scene: ctx.query.scene,
-      action: ctx.query.action,
-      actionType: ctx.query.action.replace(/(.+?)(?=[A-Z])/g, a => `${a}_`).toUpperCase(),
-    }),
-    to: ctx.query.scene ?
-      `src/scenes/${ctx.query.scene}/data/${ctx.params.name}` :
-      'src/actions',
-  }),
+  'container/:name': ({ params, query }, cb) => {
+    let to = `src/containers/${params.name}`;
+    if (query.scene) {
+      to = `src/scenes/${query.scene}/containers/${params.name}`;
+    }
+    cb({
+      from: path.resolve(__dirname, 'containers'),
+      handlePathName: name => name.replace(/__name__(?=\.)/, query.name || params.name),
+      handleContent: content => Handlebars.compile(content)({ name: params.name }),
+      to,
+    });
+  },
+  'container/component/:name': ({ params, query }, cb) => {
+    let to = `src/containers/${params.name}`;
+    if (query.scene) {
+      to = `src/scenes/${query.scene}/containers/${params.name}`;
+    }
+    cb({
+      from: path.resolve(__dirname, 'containers'),
+      handlePathName: name => name.replace(/__name__(?=\.)/, params.name),
+      handleContent: content => Handlebars.compile(content)({ name: params.name }),
+      to,
+      complete: () => {
+        shelljs.exec(`templ create component/${params.name}?${qs.stringify(query)}`);
+      },
+    });
+  },
+  'reducer/:name': ({ query, params }, cb) => {
+    cb({
+      from: path.resolve(__dirname, 'reducer/reducer.js'),
+      handlePathName: name => (query.scene ? name : `${params.name}.js`),
+      to: query.scene ?
+        `src/scenes/${query.scene}/data/${params.name}` :
+        'src/reducers',
+      complete: () => {
+        const _query = {
+          list: query.scene ?
+            fs.readdirSync(`src/scenes/${query.scene}/data`) :
+            fs.readdirSync('src/reducers')
+              .filter(a => a !== 'index.js' && !/\.swp$/.test(a))
+              .map(a => path.basename(a, '.js')),
+          scene: query.scene,
+        };
+        shelljs.exec(`tpl update 'rootReducer?${qs.stringify(_query)}'`);
+      },
+    });
+  },
+  rootReducer: ({ query }, cb) => {
+    cb({
+      from: path.resolve(__dirname, 'reducer/rootReducer.js'),
+      handlePathName: () => (query.scene ? 'reducer.js' : 'index.js'),
+      handleContent: content => Handlebars.compile(content)({
+        scene: query.scene,
+        list: (Array.isArray(query.list) ? // eslint-disable-line
+          query.list : _.isEmpty(query.list) ? [] : [query.list]).map(a => ({
+          reducer: a,
+        })),
+      }),
+      to: query.scene ? `src/scenes/${query.scene}` : 'src/reducers',
+    });
+  },
+  'action/:name': ({ params, query }, cb) => {
+    cb({
+      from: path.resolve(__dirname, 'reducer/actions.js'),
+      handleContent: content => Handlebars.compile(content)({
+        name: params.name,
+        scene: query.scene,
+        action: query.action,
+        actionType: query.action.replace(/(.+?)(?=[A-Z])/g, a => `${a}_`).toUpperCase(),
+      }),
+      to: query.scene ?
+        `src/scenes/${query.scene}/data/${params.name}` :
+        'src/actions',
+    });
+  },
 };
