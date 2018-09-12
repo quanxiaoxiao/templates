6edd1eb22f42d9d9259b8ad869a88c14a0b092d2
quan <quan_xiaoxiao@qq.com>
2018-09-12 10:12:12 +0800

    remove koa

--- templates/koa/src/routers/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router');
-const api = require('./api');
-
-const router = new Router();
-
-router.use('/api', api.routes(), router.allowedMethods());
-
-
-module.exports = router;
